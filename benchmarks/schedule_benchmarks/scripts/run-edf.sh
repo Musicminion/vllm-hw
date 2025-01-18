56bfd1a382004ebf8bc17c5b139b34aae8d8d735
Musicminion <zhangziqian@sjtu.edu.cn>
2025-01-18 20:49:48 +0800

    fix: script env var

--- benchmarks/schedule_benchmarks/scripts/run-edf.sh
+++ benchmarks/schedule_benchmarks/scripts/run-edf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Running edf"
-
+export VLLM_LOGGING_LEVEL=DEBUG
 /home/zzq/.conda/envs/vllm_zzq/bin/vllm serve "facebook/opt-125m" --port 15432 --scheduling_policy edf > ./log.txt
 
